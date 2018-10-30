c0fd327713b61514466aad4f1bd32aa32cbd0136
MateusJesus1 <mateusd.j@hotmail.com>
2018-10-30 20:51:49 -0300

    atualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,21 @@ import { TabsPage } from '../pages/tabs/tabs';
 })
 export class MyApp {
   rootPage:any = 'SigninPage';
+  showSplash = true;
 
-  constructor(platform: Platform,
-    statusBar: StatusBar,
-    splashScreen: SplashScreen,
-    private oneSignal: OneSignal) {
+  constructor(  platform: Platform,
+                statusBar: StatusBar,
+                splashScreen: SplashScreen,
+                private oneSignal: OneSignal) {
 platform.ready().then(() => {
-// Okay, so the platform is ready and our plugins are available.
-// Here you can do any higher level native things you might need.
+
 statusBar.styleDefault();
 splashScreen.hide();
 this.configurePushNotification();
 
 });
 }
+
 
 configurePushNotification(){
 window["plugins"].OneSignal
@@ -44,6 +45,7 @@ this.oneSignal.handleNotificationOpened().subscribe(() => {
 
 this.oneSignal.endInit();
 }
+
 
 }
 
